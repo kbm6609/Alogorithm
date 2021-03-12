9faadd320d316b0fb0596a861e00351165e5e305
AzureAD\구병민 <kbm6609@naver.com>
2021-03-12 16:08:55 +0900

    ver2,ver3 포함해서 수정

--- 가장많은1을가진행구하기.c
+++ 가장많은1을가진행구하기.c
@@ -48,7 +48,12 @@ int main() {
 // #어차피 1을 가장많이 갖는거를 찾는거라 굳이 모든 행들의 0번째부터 검사할필요가없다 
 // # 실행시간이 O(n)으로 대폭 감소한다.
 // # 코드 길이까지 대폭감소한다 .
+// # ver1 은 한행에서도 원소가 1일때마다 row를 바꿔주고  ver2,ver3는 한행에대해서는 카운트를 다하고 마지막에 넣어준다.
+// # ver1,ver2,ver3는 기본적인 논리는 동일하다
 #include<stdio.h>
+int ver1(int(*)[10]);
+int ver2(int(*)[10]);
+int ver3(int(*)[10]);
 int main() {
 	int arr[10][10] = {
 		{1,1,1,1,1,1,0,0,0,0},
@@ -62,22 +67,61 @@ int main() {
 		{1,1,1,0,0,0,0,0,0,0},
 		{1,1,1,1,1,1,1,0,0,0}
 	};
-	int j = 0,maxrow=0;
-	for (int i = 0; i < 10; i++) {
-		for (; j < 10; j++) {
-			if (arr[i][j] == 0) {
-				break;
-			}
-			else {
-				maxrow = i;
-			}
-		
-		}
-	
-	}
-	printf("%d", maxrow);
+	printf("ver1 : %d\n", ver1(arr));
+	printf("ver2 : %d\n", ver2(arr));
+	printf("ver3 : %d\n", ver3(arr));
 
 	return 0;
 }
+int ver1(int(*arr)[10]) {
+	int j = 0, row = 0, c = 0;
+	for (int i = 0; i < 10; i++) {
+		for (; j < 10; j++) {
+			c++;
+			if (arr[i][j] == 0) break;
+			else row = i;
+		}
+		if (j == 10) {
+			printf("ver1 실행횟수: %d\n", c);
+			return row;
+		}
+	}
+	printf("ver1 실행횟수: %d\n", c);
+	return row;
+}
+int ver2(int(*arr)[10]) {
+	int i = 0, j = 0, c = 0, row;
+	while (1) {
+		while (arr[i][j] == 1) {
+			c++;
+			j++;
+			if (j == 10) {
+				printf("ver2 반복횟수: %d\n", c);
+				return i;
+			}
+		}
+		row = i;
+		while (arr[i][j] == 0) {
+			c++;
+			i++;
+			if (i == 10) {
+				printf("ver2 반복횟수: %d\n", c);
+				return row;
+			}
+		}
+	}
+}
+int ver3(int(*arr)[10]) {
+	int i = 0, j = 0, row = 0, c = 0;
+	while ((i < 10) && (j < 10)) {
+		c++;
+		if (arr[i][j] == 0) i++;
+		else {
+			row = i;
+			j++;
+		}
+	}
+	printf("ver3 반복횟수: %d\n", c);
 
-//1번째방법도 코드길이와 변수개수를 줄일수잇지만 극적인 비교를 위해 저렇게 짜보앗다.
+	return row;
+}
